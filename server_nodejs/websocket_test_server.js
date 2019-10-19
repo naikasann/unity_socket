bf5d0578f95c0f20b4f89f70bca5da816a4a6aad
naika <name1942@gmail.com>
2019-10-19 23:56:31 +0900

    :construction: nodejsでipaddressを自動で取得するものの追加

--- server_nodejs/websocket_test_server.js
+++ server_nodejs/websocket_test_server.js
@@ -1,21 +1,32 @@
 const WebSocket  = require('ws');
- 
+var os = require("os");
+var interfaces = os.networkInterfaces();
+
+var address_list = [];
 const wss = new WebSocket.Server({ port: 3000 });
- 
+
+
 wss.on('connection', (ws) => {
+   for(var k in interfaces){
+      for(var k2 in interfaces[k]){
+         var address = interfaces[k][k2];
+         if(address.family == "IPv4" && !address.internal){
+            address_list.push(address.address);
+         }
+      }
+   }
+   console.log('Established a connection with client.');
  
-  console.log('Established a connection with client.');
+   ws.on('message', (message) => {
+      console.log(`Received a message from client: ${message}`);
+      ws.send(address_list);
+   });
  
-  ws.on('message', (message) => {
-     console.log(`Received a message from client: ${message}`);
-     ws.send("Server received message.");
-  });
- 
-setInterval(function() {
-   console.log("Sended");
-   wss.clients.forEach(client => {
-     client.send('Send to established all client.');
-  });
- }, 500);
+   setInterval(function() {
+      console.log("Sended");
+      wss.clients.forEach(client => {
+         client.send('Send to established all client.');
+      });
+   }, 1000);
  
 });
