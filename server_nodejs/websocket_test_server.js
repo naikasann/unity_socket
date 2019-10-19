342317c39ab8b70489f401a86b9ab7446b0cc5df
naika <name1942@gmail.com>
2019-10-20 01:39:54 +0900

    :construction: nodejsのip取得を適切な位置に
    関数化してそいつを呼び出すようにしたほうがいいかも。
    UnityのIPとwebsocketのIPをどのように関連付けるのか調べる。
    (IPアドレスの知識がなさすぎる)

--- server_nodejs/websocket_test_server.js
+++ server_nodejs/websocket_test_server.js
@@ -3,18 +3,26 @@ var os = require("os");
 var interfaces = os.networkInterfaces();
 
 var address_list = [];
-const wss = new WebSocket.Server({ port: 3000 });
 
-
-wss.on('connection', (ws) => {
-   for(var k in interfaces){
-      for(var k2 in interfaces[k]){
-         var address = interfaces[k][k2];
-         if(address.family == "IPv4" && !address.internal){
-            address_list.push(address.address);
-         }
+// taken ip address for unity send data
+for(var k in interfaces){
+   for(var k2 in interfaces[k]){
+      var address = interfaces[k][k2];
+      if(address.family == "IPv4" && !address.internal){
+         address_list.push(address.address);
       }
    }
+}
+
+console.log(address_list);
+
+// craate websocket. WebSocket.Server(<IPAdress> : <Port>)
+// ex :) WebSocket.Server("ws://127.0.0.1:5001")
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+//websocketserver connection callback
+wss.on('connection', (ws) => {
    console.log('Established a connection with client.');
  
    ws.on('message', (message) => {
@@ -28,5 +36,4 @@ wss.on('connection', (ws) => {
          client.send('Send to established all client.');
       });
    }, 1000);
- 
 });
