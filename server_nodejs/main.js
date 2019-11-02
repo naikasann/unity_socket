a898a53ae8b2da21568ba93bb7e89b55c0c4ccea
naika <name1942@gmail.com>
2019-11-03 00:34:44 +0900

    :truck: nodejsのfolderの作成

--- server_nodejs/main.js
+++ server_nodejs/main.js
@@ -1,5 +1,5 @@
-var export_websokcet = require("./websocket_server.js");
-var export_http = require("./http_server.js");
+var export_websokcet = require("./node_js/websocket_server");
+var export_http = require("./node_js/http_server");
 
 export_http.CreateHttpServer();
 export_websokcet.CreateWebsocketServer();
