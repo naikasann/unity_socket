bf2598c559e60ff7a4926a4a914e066b8560bed3
naika <name1942@gmail.com>
2019-11-06 17:18:25 +0900

    :sparkles: 接続者番号とモーションリストの二次配列の作成
    これを削除してモーションリストを開放する仕組みを追加する。

--- server_nodejs/node_js/websocket_server.js
+++ server_nodejs/node_js/websocket_server.js
@@ -1,13 +1,15 @@
 const WebSocket  = require('ws');
+motion_num = 10;
 
 function getSecureRandom(){
-    var random_motion = Math.floor( Math.random() * 11 );
+    var random_motion = Math.floor( Math.random() * (motion_num + 1) );
   
     return random_motion;
 }
 
 exports.CreateWebsocketServer = function () {
     var connection_list = [];
+    var connect_list = [];
     var motion_list = [];
     var unity_send = false;
 
@@ -17,39 +19,42 @@ exports.CreateWebsocketServer = function () {
     wss.on('connection', (ws) => {
         console.log('Established a connection with client.');
         connection_list.push(ws);
-        random_motion = getSecureRandom() % 10;
+        random_motion = getSecureRandom() % motion_num;
         //console.log(connection_list);
 
         ws.on('message', (message) => {
-            console.log(`${message}`);
-            var receive = ${message};
+            var receive = message;
+            console.log(receive);
             //unityからの送信の場合処理を行わない。
             if(ws == connection_list[0]){
                 //unity側からの処理
                 console.log("unity_process");
-                switch(String(receive)){
-                    //送られてきたモーション番号と一致しているか探し、送信されているコネクションリストと紐づけ。
-                    case "1":
-                        break;
-                    case "2":
-                        break;
-                }
+                //送られてきたモーション番号と一致しているか探し、送信されているコネクションリストと紐づけ。
+            
+            
             }else{
                 //デバイスからの処理
                 console.log("guest deveice process")
                 // モーションの数から配列がオーバーした場合。-1を送信してエラー処理を行う。
-                if(motion_list.length >= 10){
+                if(motion_list.length >= motion_num){
                     console.log("req max array...");
                     ws.send(-1);
                 }else{
                     // 重複しないモーションの番号を探し、それを格納する。
-                    // [接続番号：モーション番号]
                     while(motion_list.indexOf(random_motion) >= 0){
-                        random_motion = getSecureRandom() % 10;
+                        random_motion = getSecureRandom() % motion_num;
                     }
+                    for(var i = 0; i < motion_num; i++){
+                        if(ws == connection_list[i]){
+                            break;
+                        }
+                    }
+                    // connect_list = [接続番号：モーション番号]
+                    connect_list.push([i, random_motion]);
+                    console.log(connect_list);
                     motion_list.push(random_motion);
                     ws.send(random_motion);
-                    //send unity data...
+                    // send unity data...
                     connection_list[0].send(String(random_motion));
                 }
             }
