f4ddea0428ddcdb63481a9aaa830cab2de2ef2d1
naika <name1942@gmail.com>
2019-11-07 11:29:46 +0900

    :sparkles: unityに受信した[connectionnumber,motionnumber]を格納するreqestlistの作成
    次はunityで検知した場合のデータ送信処理を作成

--- websocket_for_unity_to_js/Assets/Client.cs
+++ websocket_for_unity_to_js/Assets/Client.cs
@@ -8,6 +8,7 @@ public class Client : MonoBehaviour
 {
     WebSocket ws;
     private List<string> receive_list = new List<string>();
+    private List<string[]> request_list = new List<string[]>();
 
     void Start(){
         string temp_receive;
@@ -24,14 +25,20 @@ public class Client : MonoBehaviour
             temp_receive = e.Data;
             //リストの初期化
             receive_list.Clear();
+            request_list.Clear();
             //配列をリストにコピー
             for(int i = 0; i < temp_receive.Length; i++){
                 if(!(i % 2 == 1)){
                     receive_list.Add(temp_receive[i].ToString());
                 }
             }
+            //Requestlist <= {[member_number][motion_list]}
+            for(int i = 0; i< receive_list.Count; i += 2){
+                string[] tmp = {receive_list[i], receive_list[i+1]};
+                request_list.Add(tmp);
+            }
             //配列の表示
-            showlist(receive_list);
+            showlist_(request_list);
         };
  
         ws.OnClose += (sender, e) =>
@@ -60,10 +67,22 @@ public class Client : MonoBehaviour
         ws = null;
     }
 
-    void showlist(IReadOnlyCollection<string> entities){
+    /******************************************************************************
+    *                   debug function
+     *****************************************************************************/
+    void showlist(IReadOnlyCollection<string> check_list){
         string log = "";
-        foreach(string dir in entities){
+        foreach(string dir in check_list){
             log += dir.ToString() + ",";
+        }
+        Debug.Log(log);
+    }
+    void showlist_(IReadOnlyCollection<string[]> check_list){
+        string log = "";
+        foreach(string[] dir in check_list){
+            foreach(string dirr in dir){
+                log += dirr + ",";
+            }
         }
         Debug.Log(log);
     }
