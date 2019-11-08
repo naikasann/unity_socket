a6ecfacbb5856df3ea7a120d11f72698617e002a
naika <name1942@gmail.com>
2019-11-08 11:54:01 +0900

    :heavy_plus_sign: main.jsにrequireの追加
    テスト

--- server_nodejs/main.js
+++ server_nodejs/main.js
@@ -1,3 +1,7 @@
+const http = require('http');
+const fs = require('fs');
+const WebSocket  = require('ws');
+
 var export_websokcet = require("./node_js/websocket_server");
 var export_http = require("./node_js/http_server");
 
