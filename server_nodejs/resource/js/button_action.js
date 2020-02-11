56067d7cda2b67494da4b75d83ca0dc1d25e9d64
naika <name1942@gmail.com>
2020-02-11 22:02:18 +0900

    :wrench: IPアドレスの変更

--- server_nodejs/resource/js/button_action.js
+++ server_nodejs/resource/js/button_action.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://192.168.11.4:443");
+const ws = new WebSocket("ws://127.0.0.1:443");
 
 img = new Array("../resource/img/motion/seikou_banzai_man.png",
                 "../resource/img/motion/businesswoman5_ureshii.png",
